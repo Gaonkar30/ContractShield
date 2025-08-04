a79a61ebb62387c810e417890e57071306721cd0
AkashGGaonkar <akashggaonkar@gmail.com>
2025-08-04 13:05:00 +0530

    intitial files

---
+++ demo/src/main/java/com/contractshield/demo/controller/ContractController.java
@@ -0,0 +1,11 @@
+package com.contractshield.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ContractController {
+    // We will add methods here in the next steps.
+    
+}
